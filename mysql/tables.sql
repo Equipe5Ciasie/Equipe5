c5fb2769907e1e69aa19e3a956579191b418b04e
kanuyo <leon.rohrbacher8@etu.univ-lorraine.fr>
2018-12-07 18:42:14 +0100

    finished first version of sql script

--- mysql/tables.sql
+++ mysql/tables.sql
@@ -1,5 +1,5 @@
+-- table USERS
 DROP TABLE IF EXISTS `users`;
-
 CREATE TABLE `users`(
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `email` text NOT NULL,
@@ -7,4 +7,28 @@ CREATE TABLE `users`(
     `account_type` int(11) NOT NULL, 
     PRIMARY KEY (`id`)
 );
+INSERT INTO `users`(`id`, `email`, `password`, `account_type`) VALUES
+(1, 'root', '$2y$10$tMpttxj11mBpb2vAQclug.bz5V41qKTiSRF1KBdbJzuKTYMXiwJiK', 3);
 
+-- table APPELS_OFFRES
+DROP TABLE IF EXISTS `appels_offres`;
+CREATE TABLE `appels_offres`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `titre` text NOT NULL,
+    `description` text NOT NULL,
+    `contenu` text NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+-- table METIERS
+DROP TABLE IF EXISTS `metiers`;
+CREATE TABLE `metiers`(
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `nom` text NOT NULL,
+    PRIMARY KEY (`id`)
+);
+INSERT INTO `metiers`(`id`, `nom`) VALUES
+(1, "Architecte"),
+(2, "Masson"),
+(3, "Bureau d'étude"),
+(4, "Maire");
